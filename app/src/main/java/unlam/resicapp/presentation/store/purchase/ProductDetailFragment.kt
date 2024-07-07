5a61d31bb1b0733bcf67ff42803eb7c318383cff
Leonardo Toloza <leo_eltolo@hotmail.com>
2024-07-07 14:37:17 -0300

    se agrego la funcionalidad de visualizar el detalle del producto en ProductDetailFragment

--- app/src/main/java/unlam/resicapp/presentation/store/purchase/ProductDetailFragment.kt
+++ app/src/main/java/unlam/resicapp/presentation/store/purchase/ProductDetailFragment.kt
@@ -7,25 +7,78 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.resicappandroid.databinding.FragmentProductDetailBinding
+import unlam.resicapp.data.Product
+import unlam.resicapp.presentation.util.ImageTransformation
 
 class ProductDetailFragment : Fragment() {
-
+    // ToDo:: -6- *ProductDetailFragment* / Priority: Alta
+    // Description: Mostrar producto seleccionado y permitir confirmar compra
     private var _binding: FragmentProductDetailBinding? = null
     private val binding get() = _binding!!
     private val args: ProductDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
-
         _binding = FragmentProductDetailBinding.inflate(inflater, container, false)
-
-        // ToDo:: -6- *ProductDetailFragment* / Priority: Alta
-        // Description: Mostrar rv con los productos disponibles y permitir confirmar compra en otro fragment
-        val product = args.product
         return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val product = args.product
+        setupViews(product)
+
+        binding.buyButton.setOnClickListener{
+
+        }
+
+    }
+    private fun setupViews(product: Product) {
+    // imagen del producto
+        val productImgUrl = product.logo
+        val image = ImageTransformation(productImgUrl)
+        image.loadItemSizeImage(binding.productLogo)
+
+    // Nombre del producto
+        val productNameTextView = binding.productName
+        productNameTextView.text = product.name
+
+    // Autor
+        val productAuthorTextView = binding.productAuthor
+        val productAuthor = "Autor: ${product.author}"
+        productAuthorTextView.text = productAuthor
+
+    // Tipo de producto
+        val productTypeTextView = binding.productType
+        val productType = "Tipo: ${product.type}"
+        productTypeTextView.text = productType
+
+    // Clasificación del producto
+        val productClassificationTextView = binding.productClassification
+        val productClassification = "Clasificación: ${product.classification}"
+        productClassificationTextView.text = productClassification
+
+    // Fecha de lanzamiento
+        val productReleasedDateTextView = binding.productReleasedDate
+        val productReleasedDate = "Fecha de lanzamiento: ${product.releasedDate}"
+        productReleasedDateTextView.text = productReleasedDate
+
+    // Categoría
+        val productCategoryTextView = binding.productCategory
+        val productCategory = "Categoría: ${product.category}"
+        productCategoryTextView.text = productCategory
+
+    // Estrellas
+        val productStarsTextView = binding.productStars
+        val productStars = "Estrellas: ${product.stars}"
+        productStarsTextView.text = productStars
+
+    // Precio
+        val productPriceTextView = binding.productPrice
+        val productPrice = "Precio: ${product.price}"
+        productPriceTextView.text = productPrice
     }
 
     override fun onDestroyView() {
