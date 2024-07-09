fca6ebf19a28293548a37d7ee6e6953a6f35b8ff
Leonardo Toloza <leo_eltolo@hotmail.com>
2024-07-08 21:14:12 -0300

    optimizacion de imports

--- app/src/main/java/unlam/resicapp/MainActivity.kt
+++ app/src/main/java/unlam/resicapp/MainActivity.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
 import com.example.resicappandroid.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var appBarConfiguration: AppBarConfiguration
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
